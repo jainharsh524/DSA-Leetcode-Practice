e0406e112a2a3d89957e69e6c96eb98528ab5ca2
jainharsh524 <harshnareshjain1729@gmail.com>
2025-04-21 01:51:12 +0530

    Question added

---
+++ src/Arrays/Easy/flippingAnImage.java
@@ -0,0 +1,34 @@
+package Arrays.Easy;
+
+//Q.832 https://leetcode.com/problems/flipping-an-image/description/
+public class flippingAnImage {
+    static int[] flip(int[] arr){
+        for(int i =0;i<arr.length/2;i++){
+            int temp = arr[i];
+            arr[i] = arr[arr.length-i-1];
+            arr[arr.length-i-1] = temp;
+        }
+        return arr;
+    }
+    static int[][] invert(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j]==0) arr[i][j]=1;
+                else arr[i][j] = 0;
+            }
+        }
+        return arr;
+    }
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int i=0;i<image.length;i++){
+            image[i] = flip(image[i]);
+        }
+        image = invert(image);
+        return image;
+    }
+    //# Complexity
+    //- Time complexity:O(N)
+    public static void main(String[] args) {
+
+    }
+}
