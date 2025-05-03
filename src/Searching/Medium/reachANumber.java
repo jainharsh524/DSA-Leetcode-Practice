cefdb0a84465ec54fafc08a13bd7b38d8b1bc042
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-03 18:34:43 +0530

    Question added

--- src/Searching/Medium/reachANumber.java
+++ src/Searching/Medium/reachANumber.java
@@ -6,16 +6,29 @@ public class reachANumber {
         int steps = 1;
         int currentValue = 0;
         int moves = 0;
-        while(currentValue<Math.abs(target)){
-            if(currentValue+steps > Math.abs(target)) currentValue = currentValue- steps;
-            else currentValue = currentValue+ steps;
-            steps++; moves++;
+        target = Math.abs(target);
+        while(true){
+            if(currentValue>=target) {
+                if ((currentValue - target) % 2 != 0) {
+                    currentValue = currentValue + steps;
+                    steps++;
+                    moves++;
+                } else break;
+            }
+            else{
+                currentValue = currentValue + steps;
+                steps++;
+                moves++;
+            }
         }
         return moves;
     }
     public static void main(String[] args) {
-        System.out.println(reachNumber(5));
-        //0>1>3
-        System.out.println(Math.abs(-1));
+        System.out.println(reachNumber(9));
+//        System.out.println(Math.abs(-1));
+        //0>1>3>6>10
+        //0>-1>1>4>4
+        //0>1>3>6>10>5
+        //0>1>3>6
     }
 }
