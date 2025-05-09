6c716b15be7045c90cf8f1a5dfedfa4a2440bfd2
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-09 22:43:25 +0530

    Question added

--- src/Sorting/Easy/containsDuplicate.java
+++ src/Sorting/Easy/containsDuplicate.java
@@ -1,6 +1,18 @@
 package Sorting.Easy;
 
+import java.util.Arrays;
+
 //Q.217 https://leetcode.com/problems/contains-duplicate/description/
 public class containsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i]==nums[i-1]) return true;
+        }
+        return false;
+    }
 
+    public static void main(String[] args) {
+
+    }
 }
