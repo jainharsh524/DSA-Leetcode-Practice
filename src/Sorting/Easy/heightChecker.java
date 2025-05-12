e0b2be6f7771a4416c23d6727aef00fa434bb98e
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-12 17:55:34 +0530

    Question added

--- src/Sorting/Easy/heightChecker.java
+++ src/Sorting/Easy/heightChecker.java
@@ -1,7 +1,20 @@
 package Sorting.Easy;
+import java.util.*;
 
 //Q.1051 https://leetcode.com/problems/height-checker/description/
 public class heightChecker {
+    public static int heightChecker(int[] heights) {
+        int n = heights.length;
+        int[] sorted =  Arrays.copyOf(heights, n);
+        int count = 0;
+        Arrays.sort(sorted);
+        for (int i = 0; i < n; i++) {
+            if(sorted[i] != heights[i]) {
+                count ++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
 
     }
