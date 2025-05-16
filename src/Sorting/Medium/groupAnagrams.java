4005ed2a336010b13b607133b63136e8cc90d5be
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-16 17:28:18 +0530

    Question added

---
+++ src/Sorting/Medium/groupAnagrams.java
@@ -0,0 +1,27 @@
+package Sorting.Medium;
+
+import java.util.*;
+
+
+//Q.49 https://leetcode.com/problems/group-anagrams/description/
+public class groupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+
+            map.computeIfAbsent(sorted, k -> new ArrayList<>()).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
