7b3b70b66c2ea4412d02165bb3b879b6f1d6adda
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-22 16:21:36 +0530

    Question added

---
+++ src/String/Easy/reverseWordsInAStringIII.java
@@ -0,0 +1,29 @@
+package String.Easy;
+
+//Q.557 https://leetcode.com/problems/reverse-words-in-a-string-iii/
+public class reverseWordsInAStringIII {
+    public static void swap(char[] arr, int index1, int index2){
+        char temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+    public static String reverse(String s){
+        char[] charArray = s.toCharArray();
+        int i = 0, j = s.length()-1;
+        while (i<j){
+            swap(charArray, i, j);
+            i++;j--;
+        }
+        return String.valueOf(charArray);
+    }
+    public static String reverseWords(String s) {
+        String[] arr = s.split(" ");
+        for (int i = 0; i < arr.length; i++) arr[i] = reverse(arr[i]);
+        s = "";
+        for (int i = 0; i < arr.length; i++) s = s + arr[i]+" ";
+        return s.trim();
+    }
+    public static void main(String[] args) {
+        System.out.println(reverseWords("Let's take LeetCode contest"));
+    }
+}
