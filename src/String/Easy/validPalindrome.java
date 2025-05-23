5c3ba968028598490e9b911b1a8a89d712cca25b
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-23 17:19:04 +0530

    Question added

---
+++ src/String/Easy/validPalindrome.java
@@ -0,0 +1,29 @@
+package String.Easy;
+
+//Q.125 https://leetcode.com/problems/valid-palindrome/description/
+public class validPalindrome {
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int i =0;
+        int j =s.length()-1;
+        while(i<j){
+            if((s.charAt(i)>0&&s.charAt(i)<=47)||(s.charAt(i)>=58&&s.charAt(i)<=64)||(s.charAt(i)>=91&&s.charAt(i)<=96)||(s.charAt(i)>=123&&s.charAt(i)<127)){
+                i++;
+                continue;
+            }
+            if((s.charAt(j)>0&&s.charAt(j)<=47)||(s.charAt(j)>=58&&s.charAt(j)<=64)||(s.charAt(j)>=91&&s.charAt(j)<=96)||(s.charAt(j)>=123&&s.charAt(j)<127)){
+                j--;
+            }
+            else if(s.charAt(i)==s.charAt(j)){
+                i++;j--;
+            }
+            else if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
