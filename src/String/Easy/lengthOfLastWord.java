70b6c102aa1cee2c84b62da25b7c16b9694d7fdb
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-25 17:37:45 +0530

    Question added

---
+++ src/String/Easy/lengthOfLastWord.java
@@ -0,0 +1,22 @@
+package String.Easy;
+
+public class lengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
+        }
+
+        return length;
+    }
+    public static void main(String[] args) {
+
+    }
+}
