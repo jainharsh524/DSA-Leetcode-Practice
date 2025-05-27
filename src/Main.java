9c779ac1037c4204fbacb02d1eff8b82907a8ab6
jainharsh524 <harshnareshjain1729@gmail.com>
2025-05-27 20:50:47 +0530

    Question added

--- src/Main.java
+++ src/Main.java
@@ -148,6 +148,14 @@ public class Main {
 
         return -1;
     }
+    public int differenceOfSums(int n, int m) {
+        int num1 = 0, num2 = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) num2 += i;
+            else num1 += i;
+        }
+        return num1 - num2;
+    }
 
     public static void main(String[] args) {
 //        System.out.println(maxProduct(124));
