472b2f49d9c785b3dede425572a77976f4bf95f3
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-01 18:20:42 +0530

    Question added

--- src/String/Medium/basicCalculatorII.java
+++ src/String/Medium/basicCalculatorII.java
@@ -2,82 +2,40 @@ package String.Medium;
 
 //Q.227 https://leetcode.com/problems/basic-calculator-ii/description/
 public class basicCalculatorII {
-    public static boolean isOperator(char c){
-        return c=='+'||c=='-'||c=='*'||c=='/';
-    }
     public static int calculate(String s) {
-        s = s.replace(" ", "");
-        int res = 0;
-        int numLeft = 0;
-        int numRight = 0;
-        if(!s.contains("+")&&!s.contains("-")&&!s.contains("*")&&!s.contains("/")){
-            return Integer.parseInt(s);
-        }
-        else {
-            StringBuilder sb = new StringBuilder(s);
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (Character.isDigit(c)) {
-                    numLeft = numLeft * 10 + (c - '0');
-                }
-                if(isOperator(c)){
-                    if(c=='/'){
-                        if (Character.isDigit(c)) {
-                            numRight = numRight * 10 + (c - '0');
-                        }
-                        else {
-                            res = numLeft/numRight;
-                            numLeft=res;
-                            numRight = 0;
-                            int left = (int) (Math.log10(numLeft)+1);
-                            int right = (int) (Math.log10(numRight)+1);
-                            sb = sb.delete(i-left, i+right);
+        int num = 0;
+        char operator = '+';
+        int last = 0, sum = 0;
 
-                        }
-                    }
-                    else {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-                    }
-                }
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
             }
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '*') {
-                    int a = s.charAt(i - 1) - '0';
-                    int b = s.charAt(i + 1) - '0';
-                    res = a * b;
-                    sb.deleteCharAt(i + 1);
-                    sb.deleteCharAt(i);
-                    sb.deleteCharAt(i - 1);
-                    sb.insert(i - 1, res);
-                    s = sb.toString();
+
+            if (isOperator(c) || i == s.length() - 1) {
+                if (operator == '+') {
+                    sum += last;
+                    last = num;
                 }
-            }
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '+') {
-                    int a = s.charAt(i - 1) - '0';
-                    int b = s.charAt(i + 1) - '0';
-                    res = a + b;
-                    sb.deleteCharAt(i + 1);
-                    sb.deleteCharAt(i);
-                    sb.deleteCharAt(i - 1);
-                    sb.insert(i - 1, res);
-                    s = sb.toString();
+                else if (operator == '-') {
+                    sum += last;
+                    last = -num;
                 }
-            }
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '-') {
-                    int a = s.charAt(i - 1) - '0';
-                    int b = s.charAt(i + 1) - '0';
-                    res = a - b;
-                    sb.deleteCharAt(i + 1);
-                    sb.deleteCharAt(i);
-                    sb.deleteCharAt(i - 1);
-                    sb.insert(i - 1, res);
-                    s = sb.toString();
-                }
+                else if (operator == '*') last *= num;
+                else if (operator == '/') last /= num;
+
+                num = 0;
+                operator = c;
             }
         }
-        return res;
+
+        return sum += last;
+    }
+
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
     }
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("12345");
