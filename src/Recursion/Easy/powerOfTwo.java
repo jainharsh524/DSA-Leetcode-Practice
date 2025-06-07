def98492b81e2cd6a801e55cfb9dc202b3a61828
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-07 22:01:48 +0530

    Question added

--- src/Recursion/Easy/powerOfTwo.java
+++ src/Recursion/Easy/powerOfTwo.java
@@ -2,11 +2,14 @@ package Recursion.Easy;
 
 //Q.231 https://leetcode.com/problems/power-of-two/description/
 public class powerOfTwo {
+    //Method using Recursion
     public static boolean helper(int n, int a){
         if(a==n) return true;
         else if(a<n) return helper(n, a*2);
         else return false;
     }
+
+    //Method using Math
     public static boolean isPowerOfTwo(int n) {
 //        return helper(n, 1);
         double log2_x = Math.log10(n)/Math.log10(2);
@@ -14,6 +17,7 @@ public class powerOfTwo {
         if(str.charAt(str.length()-1)=='0'&&str.charAt(str.length()-2)=='.') return true;
         else return false;
     }
+
     //Another Method using Bitwise Operators
     public boolean isPowerOfTwo1(int n) {
         if ( n <= 0) return false;
