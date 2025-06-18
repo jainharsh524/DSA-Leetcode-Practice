6825877b155ecceb414daeea674f6720b67a56e2
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-18 10:12:47 +0530

    Question added

---
+++ src/bitwise/Easy/primeNumberOfSetBitsInBinaryRepresentation.java
@@ -0,0 +1,39 @@
+package bitwise.Easy;
+
+//Q. https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/description/
+public class primeNumberOfSetBitsInBinaryRepresentation {
+    public static int hammingWeight(int num) {
+        int count = 0;
+        while (num>0){
+            int end = num&1;
+            if(end==1) count++;
+            num = num>>1;
+        }
+        return count;
+    }
+    public static boolean isPrime(int num){
+        if(num==1) return false;
+        else if(num==2) return true;
+        else {
+            int i = 2;
+            while(i<num){
+                if(num%i==0) return false;
+                else i++;
+            }
+        }
+        return true;
+    }
+    public static int countPrimeSetBits(int left, int right) {
+        int countPrime = 0;
+        int curr = left;
+        while(curr<=right){
+            int setBits = hammingWeight(curr);
+            if(isPrime(setBits)) countPrime++;
+            curr++;
+        }
+        return countPrime;
+    }
+    public static void main(String[] args) {
+
+    }
+}
