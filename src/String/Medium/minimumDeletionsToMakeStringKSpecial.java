73af6839ab71972a0187417ede61e991efcc6e37
jainharsh524 <harshnareshjain1729@gmail.com>
2025-06-21 16:40:56 +0530

    `Question added`

---
+++ src/String/Medium/minimumDeletionsToMakeStringKSpecial.java
@@ -0,0 +1,35 @@
+package String.Medium;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+//Q.3085 https://leetcode.com/problems/minimum-deletions-to-make-string-k-special/description/?envType=daily-question&envId=2025-06-21
+public class minimumDeletionsToMakeStringKSpecial {
+    public static int minimumDeletions(String word, int k) {
+        int countMin = word.length();
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            if(!freq.containsKey(word.charAt(i))) freq.put(word.charAt(i), 1);
+            else {
+                int value = freq.get(word.charAt(i));
+                freq.replace(word.charAt(i), value, value+1);
+            }
+        }
+        for (Integer value : freq.values()) {
+            int countDel = 0;
+            for (Integer val : freq.values()) {
+                if(value>val) countDel = countDel+val;
+                else if(value+k<val){
+                    countDel = countDel+val-value-k;
+                }
+            }
+            if(countDel<countMin) countMin = countDel;
+        }
+        return countMin;
+    }
+    public static void main(String[] args) {
+        System.out.println(minimumDeletions("aabcaba", 0));
+    }
+}
